4b6168fd8a870440d9f4295811f2aaad234e1444
Gustavo Êrades Vilarinho Silva <108373134+Gustavo-erades@users.noreply.github.com>
2023-03-27 16:31:23 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,60 @@
+def inserir_aluno():
+    nome=str(input("Nome do aluno: "))
+    mat=int(input("Número de Matrícula do(a) %s: "%nome))
+    with open ("Alunos.txt","a+") as f:
+        f.write(nome+"-"+str(mat)+"\n")
+    f.close()
+    
+def excluir_aluno():
+    with open ("Alunos.txt","w+") as f:
+        f.write("")
+    f.close()
+
+def editar_aluno():
+    with open ("Alunos.txt","r+") as f:
+        print("Alunos já cadastrados: \n"+f.readline())
+        nome=str(input("Nome do aluno: "))
+        mat=int(input("Número de Matrícula do(a) %s: "%nome))
+        f.write(nome+"-"+str(mat)+"\n")
+    f.close()
+
+def listar_alunos():
+    with open ("Alunos.txt","r",encoding="UTF-8") as f:
+        print(f.read())
+    f.close()
+
+def menu():
+    print("**********************")
+    print("* 1 -> Inserir aluno *")
+    print("* 2 -> Excluir aluno *")
+    print("* 3 -> Editar aluno  *")
+    print("* 4 -> Listar alunos *")
+    print("* 5 ->   Encerrar    *")
+    print("**********************")
+def logica():    
+    match opcao:
+        case 1:
+            inserir_aluno()
+        case 2:
+            excluir_aluno()
+        case 3:
+            editar_aluno()
+        case 4:
+            listar_alunos()
+        case 5:
+            print("Fim :)")
+            exit()
+        case _:
+            print("Opção inválida!")
+menu()
+opcao=int(input("Opção escolhida: "))
+logica()        
+while(opcao!=5):
+    resp=str(input("Deseja continuar (S/N)? "))
+    if(resp=="S" or resp=="s"):
+        menu()
+        opcao=int(input("Opção escolhida: "))
+        logica()
+    else:
+        print("Fim :)")
+        exit()
